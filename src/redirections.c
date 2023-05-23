f7f75ae257aaac794452439fb1c45f37bfeaef26
kpp <you@example.com>
2023-05-23 11:36:35 -0400

    .

--- src/redirections.c
+++ src/redirections.c
@@ -4,70 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int open_output_file(char *filename, int flags, mode_t mode);
-void handle_input_redirection(char **argv, int *argc, char **input_file);
-void handle_output_redirection(char **argv, int *argc, char **output_file, char **append_file);
-void handle_heredoc_redirection(char **argv, int *argc, char **delimiter);
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <command>\n", argv[0]);
-        return 1;
-    }
-
-    int input_fd = STDIN_FILENO;
-    int output_fd = STDOUT_FILENO;
-    char *input_file = NULL;
-    char *output_file = NULL;
-    char *append_file = NULL;
-    char *delimiter = NULL;
-
-    handle_input_redirection(argv, &argc, &input_file);
-    handle_output_redirection(argv, &argc, &output_file, &append_file);
-    handle_heredoc_redirection(argv, &argc, &delimiter);
-
-    if (input_file != NULL) {
-        input_fd = open(input_file, O_RDONLY);
-        if (input_fd == -1) {
-            perror("open");
-            return 1;
-        }
-    }
-
-    if (output_file != NULL) {
-        output_fd = open_output_file(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (output_fd == -1) {
-            perror("open");
-            return 1;
-        }
-    } else if (append_file != NULL) {
-        output_fd = open_output_file(append_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-        if (output_fd == -1) {
-            perror("open");
-            return 1;
-        }
-    }
-
-    if (dup2(input_fd, STDIN_FILENO) == -1) {
-        perror("dup2");
-        return 1;
-    }
-    if (dup2(output_fd, STDOUT_FILENO) == -1) {
-        perror("dup2");
-        return 1;
-    }
-
-    system(argv[1]);
-
-    if (input_file != NULL) {
-        close(input_fd);
-    }
-    if (output_file != NULL || append_file != NULL) {
-        close(output_fd);
-    }
-
-    return 0;
-}
 
 int open_output_file(char *filename, int flags, mode_t mode) {
     int fd = open(filename, flags, mode);
@@ -139,4 +75,64 @@ void handle_heredoc_redirection(char **argv, int *argc, char **delimiter) {
             free(line);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command>\n", argv[0]);
+        return 1;
+    }
+
+    int input_fd = STDIN_FILENO;
+    int output_fd = STDOUT_FILENO;
+    char *input_file = NULL;
+    char *output_file = NULL;
+    char *append_file = NULL;
+    char *delimiter = NULL;
+
+    handle_input_redirection(argv, &argc, &input_file);
+    handle_output_redirection(argv, &argc, &output_file, &append_file);
+    handle_heredoc_redirection(argv, &argc, &delimiter);
+
+    if (input_file != NULL) {
+        input_fd = open(input_file, O_RDONLY);
+        if (input_fd == -1) {
+            perror("open");
+            return 1;
+        }
+    }
+
+    if (output_file != NULL) {
+        output_fd = open_output_file(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (output_fd == -1) {
+            perror("open");
+            return 1;
+        }
+    } else if (append_file != NULL) {
+        output_fd = open_output_file(append_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (output_fd == -1) {
+            perror("open");
+            return 1;
+        }
+    }
+
+    if (dup2(input_fd, STDIN_FILENO) == -1) {
+        perror("dup2");
+        return 1;
+    }
+    if (dup2(output_fd, STDOUT_FILENO) == -1) {
+        perror("dup2");
+        return 1;
+    }
+
+    system(argv[1]);
+
+    if (input_file != NULL) {
+        close(input_fd);
+    }
+    if (output_file != NULL || append_file != NULL) {
+        close(output_fd);
+    }
+
+    return 0;
 }

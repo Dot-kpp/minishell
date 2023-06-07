f791cc6d8f8d1dc5c6458f798304feab44219757
kpp <you@example.com>
2023-06-07 11:01:01 -0400

    clean up

--- src/redirections.c
+++ src/redirections.c
@@ -27,66 +27,76 @@ int open_output_file(char *filename, int flags, mode_t mode) {
 }
 
 void handle_input_redirection(char **argv, int *argc, char **input_file) {
-    for (int i = 0; i < *argc; i++) {
-        if (strcmp(argv[i], "<") == 0) {
+    int i = 0;
+    while (i < *argc) {
+        if (ft_strncmp(argv[i], "<", 1) == 0) {
             if (i + 1 >= *argc) {
-                fprintf(stderr, "Error: no input file specified\n");
+                char *error_message = "Error: no input file specified\n";
+                write(STDERR_FILENO, error_message, ft_strlen(error_message));
                 exit(1);
             }
             *input_file = argv[i + 1];
-            memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
+            ft_memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
             *argc -= 2;
             i--;
         }
+        i++;
     }
 }
 
 void handle_output_redirection(char **argv, int *argc, char **output_file, char **append_file) {
-    for (int i = 0; i < *argc; i++) {
-        if (strcmp(argv[i], ">") == 0) {
+    int i = 0;
+    while (i < *argc) {
+        if (ft_strncmp(argv[i], ">", 1) == 0) {
             if (i + 1 >= *argc) {
-                fprintf(stderr, "Error: no output file specified\n");
+                char *error_message = "Error: no output file specified\n";
+                write(STDERR_FILENO, error_message, ft_strlen(error_message));
                 exit(1);
             }
             *output_file = argv[i + 1];
-            memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
+            ft_memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
             *argc -= 2;
             i--;
-        } else if (strcmp(argv[i], ">>") == 0) {
+        } else if (ft_strncmp(argv[i], ">>", 2) == 0) {
             if (i + 1 >= *argc) {
-                fprintf(stderr, "Error: no output file specified\n");
+                char *error_message = "Error: no output file specified\n";
+                write(STDERR_FILENO, error_message, ft_strlen(error_message));
                 exit(1);
             }
             *append_file = argv[i + 1];
-            memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
+            ft_memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
             *argc -= 2;
             i--;
         }
+        i++;
     }
 }
 
 void handle_heredoc_redirection(char **argv, int *argc, char **delimiter) {
-    for (int i = 0; i < *argc; i++) {
-        if (strcmp(argv[i], "<<") == 0) {
+    int i = 0;
+    while (i < *argc) {
+        if (ft_strncmp(argv[i], "<<", 2) == 0) {
             if (i + 1 >= *argc) {
-                fprintf(stderr, "Error: no delimiter specified\n");
+                char *error_message = "Error: no delimiter specified\n";
+                write(STDERR_FILENO, error_message, ft_strlen(error_message));
                 exit(1);
             }
             *delimiter = argv[i + 1];
-            memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
+            ft_memmove(&argv[i], &argv[i + 2], (*argc - i - 1) * sizeof(char *));
             *argc -= 2;
             i--;
             char *line = NULL;
             size_t len = 0;
             ssize_t nread;
             while ((nread = getline(&line, &len, stdin)) != -1) {
-                if (strcmp(line, *delimiter) == 0) {
+                if (ft_strncmp(line, *delimiter, ft_strlen(*delimiter)) == 0) {
                     break;
                 }
                 write(STDOUT_FILENO, line, nread);
             }
             free(line);
         }
+        i++;
     }
 }
 
@@ -111,7 +121,6 @@ int call_redirections(t_cmd *cmd, t_mshell *mshell)
             return -1;
         }
     }
-
     if (output_file != NULL) {
         output_fd = open_output_file(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (output_fd == -1) {
@@ -125,7 +134,6 @@ int call_redirections(t_cmd *cmd, t_mshell *mshell)
             return -1;
         }
     }
-
     if (dup2(input_fd, STDIN_FILENO) == -1) {
         perror("dup2");
         return -1;
@@ -134,13 +142,6 @@ int call_redirections(t_cmd *cmd, t_mshell *mshell)
         perror("dup2");
         return -1;
     }
-
-    // exit_status = call_builtin(cmd.argc, (const char **)cmd.argv, mshell);
-    // if (exit_status > -1) {
-    //     return exit_status;
-    // }
-    // exit_status = execve(expand_cmd(cmd.argv[0], ms_getenv("PATH", (const char **)mshell->env)), cmd.argv, mshell->env);
-
     if (input_file != NULL) {
         close(input_fd);
     }
@@ -149,63 +150,3 @@ int call_redirections(t_cmd *cmd, t_mshell *mshell)
     }
     return -1;
 }
-
-// int main(int argc, char *argv[]) {
-//     if (argc < 2) {
-//         fprintf(stderr, "Usage: %s <command>\n", argv[0]);
-//         return 1;
-//     }
-
-//     int input_fd = STDIN_FILENO;
-//     int output_fd = STDOUT_FILENO;
-//     char *input_file = NULL;
-//     char *output_file = NULL;
-//     char *append_file = NULL;
-//     char *delimiter = NULL;
-
-//     handle_input_redirection(argv, &argc, &input_file);
-//     handle_output_redirection(argv, &argc, &output_file, &append_file);
-//     handle_heredoc_redirection(argv, &argc, &delimiter);
-
-//     if (input_file != NULL) {
-//         input_fd = open(input_file, O_RDONLY);
-//         if (input_fd == -1) {
-//             perror("open");
-//             return 1;
-//         }
-//     }
-
-//     if (output_file != NULL) {
-//         output_fd = open_output_file(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-//         if (output_fd == -1) {
-//             perror("open");
-//             return 1;
-//         }
-//     } else if (append_file != NULL) {
-//         output_fd = open_output_file(append_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-//         if (output_fd == -1) {
-//             perror("open");
-//             return 1;
-//         }
-//     }
-
-//     if (dup2(input_fd, STDIN_FILENO) == -1) {
-//         perror("dup2");
-//         return 1;
-//     }
-//     if (dup2(output_fd, STDOUT_FILENO) == -1) {
-//         perror("dup2");
-//         return 1;
-//     }
-
-//     system(argv[1]);
-
-//     if (input_file != NULL) {
-//         close(input_fd);
-//     }
-//     if (output_file != NULL || append_file != NULL) {
-//         close(output_fd);
-//     }
-
-//     return 0;
-//     }
